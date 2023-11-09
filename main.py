24b181c7124700d2a70dbee081a821db0a5191a5
Deniz Işık <denizisik2004@gmail.com>
2023-11-09 21:03:02 +0300

    Add csv files and bing_api

--- main.py
+++ main.py
@@ -4,4 +4,7 @@ from utils import generate_city
 for k in range(5):
     cities = generate_city.generate_random_cities(20)
     generate_csv.generate_sus_csv(k, cities)
-    generate_csv.generate_normal_csv(k, cities)
+
+for j in range(20):
+    cities = generate_city.generate_random_cities(20)
+    generate_csv.generate_normal_csv(j, cities)
