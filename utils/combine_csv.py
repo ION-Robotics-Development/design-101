be787650db1711fb9bd8bb8d196226d26093a1ba
Deniz Işık <denizisik2004@gmail.com>
2023-11-11 01:41:20 +0300

    Update combine utilities

--- utils/combine_csv.py
+++ utils/combine_csv.py
@@ -1,4 +1,3 @@
-
 import pandas as pd
 
 # read in the two csv files
@@ -14,6 +13,24 @@ susp_df['type'] = 'scammer'
 
 # concatenate the two dataframes
 combined_df = pd.concat([normal_df, susp_df], ignore_index=True)
+
+# read in the Normal.csv file
+test_df = pd.read_csv('./CSV/Normal.csv')
+
+# modify the Caller ID column to be 21
+test_df['Caller ID'] = 21
+
+# add a new column 'type' to the test dataframe
+test_df['type'] = 'test'
+
+# concatenate the test dataframe with the combined dataframe
+combined_df = pd.concat([combined_df, test_df], ignore_index=True)
+
+# group the dataframe by Caller ID and apply a function to each group
+combined_df = combined_df.groupby('Caller ID').apply(lambda x: x.sample(frac=0.5))
+
+# reset the index of the dataframe
+combined_df = combined_df.reset_index(drop=True)
 
 # write the combined dataframe to a new csv file
 combined_df.to_csv('./CSV/combined.csv', index=False)
